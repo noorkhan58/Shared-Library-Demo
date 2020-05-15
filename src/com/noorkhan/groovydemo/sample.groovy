89f864d5123814f50a968a7ca4d5b6d18d23d913
Noor Khan <noorrezoan1988@yahoo.com>
2020-05-15 15:50:36 -0700

    test

--- src/com/noorkhan/groovydemo/sample.groovy
+++ src/com/noorkhan/groovydemo/sample.groovy
@@ -1,9 +1,21 @@
 
 package com.noorkhan.groovydemo
-import com.noorkhan.groovydemo.pipelinetest
+import com.noorkhan.groovydemo.gitCheckout
  class sample {
 	
 	void test() {
-		pipelinetest
+		pipeline {
+			agent any
+				stages {
+					stage('Git Checkout') {
+						steps {
+							gitCheckout(
+								branch: "master", 
+								url: "https://github.com/spring-projects/spring-petclinic.git"
+							)
+						}
+					}
+				}
+			}
 	}
 }
