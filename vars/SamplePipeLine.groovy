39a5a8bef5bbae842a4ab0c9e35330a5e262e372
Noor Khan <noorrezoan1988@yahoo.com>
2020-05-15 16:08:02 -0700

    test

---
+++ vars/SamplePipeLine.groovy
@@ -0,0 +1,19 @@
+import com.noorkhan.groovydemo.gitCheckout
+
+def call() {
+	pipeline {
+			agent any
+				stages {
+					stage('Git Checkout') {
+						steps {
+							echo 'Hello World'
+							gitCheckout(
+								branch: "master",
+								url: "https://github.com/spring-projects/spring-petclinic.git"
+							)
+						}
+					}
+				}
+			}
+	
+}
